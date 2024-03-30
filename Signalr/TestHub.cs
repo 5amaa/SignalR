6e99db05b898d1aa6ff588c6962dbff53e3af7f4
5amaa <135070852+5amaa@users.noreply.github.com>
2024-03-30 03:44:06 +0200

    k

---
+++ Signalr/TestHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Signalr
+{
+	public class TestHub : Hub
+	{
+		private readonly IHubContext<TestHub> _hubContext;
+
+    public TestHub(IHubContext<TestHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+		// Generic method to broadcast a message to all clients
+		public async Task Broadcast(string eventName, object data)
+		{
+			// Send the message to all clients connected to the hub
+			await Clients.All.SendAsync(eventName, data);
+		}
+	}
+}
---
+++ test/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace test
+{
+	[HubName("chat")]
+	public class ChatHub : Hub
+	{
+		public void sendMessage(string name, string mess)
+		{
+			 Clients.All.newMessage(name, mess);
+		}
+	}
+}
